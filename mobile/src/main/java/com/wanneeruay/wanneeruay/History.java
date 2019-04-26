bb1fa1a4e438e568e612f5852739231a603ac86a
hiiamtin <60010866@kmitl.ac.th>
2019-04-26 12:40:13 +0700

    -0-

--- mobile/src/main/java/com/wanneeruay/wanneeruay/History.java
+++ mobile/src/main/java/com/wanneeruay/wanneeruay/History.java
@@ -9,18 +9,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.wanneeruay.wanneeruay.Firebase.FirebaseHelper;
-import com.wanneeruay.wanneeruay.Firebase.Spacecraft;
+import java.util.ArrayList;
 
-public class History extends AppCompatActivity implements View.OnClickListener{
+public class History extends AppCompatActivity implements View.OnClickListener,AdapterView.OnItemSelectedListener{
 
     EditText number;
+    ArrayList<String> date=Menu.date;
     static String readQr ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +33,19 @@ public class History extends AppCompatActivity implements View.OnClickListener{
         final ConstraintLayout ct = findViewById(R.id.constraintLayout);
         final Button btMoney = findViewById(R.id.manage_money_bt);
         final Button btQr = findViewById(R.id.Qrbut);
+        final Spinner dateSp = findViewById(R.id.spinner_date_H);
+
+        dateSp.setAdapter(updateSpiner());
+        dateSp.setOnItemSelectedListener(this);
+
         number.setBackgroundTintMode(PorterDuff.Mode.ADD);
         number.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.colorNOTOK,getTheme())));
+
         btConferm.setOnClickListener(this);
         btMoney.setOnClickListener(this);
         ct.setOnClickListener(this);
         btQr.setOnClickListener(this);
+
         number.setOnFocusChangeListener((v, hasFocus) -> {
             if(!hasFocus) {
                 checkErrorTextInput(v);
@@ -90,6 +98,17 @@ public class History extends AppCompatActivity implements View.OnClickListener{
         }
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),text,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
     public void showKeyboard(View view){
         InputMethodManager imm = (InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
         imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT);
@@ -98,6 +117,14 @@ public class History extends AppCompatActivity implements View.OnClickListener{
         InputMethodManager imm = (InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
+    private ArrayAdapter<String> updateSpiner(){
+        ArrayAdapter<String> data = new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item,date);
+        data.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //ArrayAdapter<CharSequence> dateSelecAp = ArrayAdapter.createFromResource(this,R.array.lottary_date,android.R.layout.simple_spinner_item);
+        //dateSelecAp.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        return data;
+    }
+
     public  void toastprint() {
         Toast toast = Toast.makeText(getApplicationContext(), readQr, Toast.LENGTH_LONG);
         toast.setGravity(0, 0, 0);
