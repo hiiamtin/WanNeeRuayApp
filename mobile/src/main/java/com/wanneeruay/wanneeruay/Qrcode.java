a430bc25764586fd822a9c7353d95e3220a8fee0
None-passakorn <non.ty123@hotmail.com>
2019-05-01 17:35:57 +0700

    before 6.00

--- mobile/src/main/java/com/wanneeruay/wanneeruay/Qrcode.java
+++ mobile/src/main/java/com/wanneeruay/wanneeruay/Qrcode.java
@@ -2,22 +2,16 @@ package com.wanneeruay.wanneeruay;
 
 import android.Manifest;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
-import android.hardware.camera2.CameraManager;
-import android.os.Vibrator;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,26 +21,22 @@ import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import java.io.IOException;
-import java.lang.reflect.Parameter;
 
 public class Qrcode extends AppCompatActivity implements View.OnClickListener {
-    
+
     SurfaceView surfaceView;
     CameraSource cameraSource;
     BarcodeDetector barcodeDetector;
-    static TextView textView;
-    static String Qrtext;
-    CameraManager cameraManager;
+    TextView textView;
 
-    //android.graphics.Camera cam;
     boolean stop = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode);
-        surfaceView = (SurfaceView) findViewById(R.id.Camsurface);
+        surfaceView =  findViewById(R.id.Camsurface);
         barcodeDetector = new BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.QR_CODE).build();
-        textView = (TextView) findViewById(R.id.Qrstr);
+        textView = findViewById(R.id.Qrstr);
         textView.setOnClickListener(this); //test
         cameraSource = new CameraSource.Builder(this,barcodeDetector).setRequestedPreviewSize(640,480).setAutoFocusEnabled(true).build();
         surfaceView.setOnClickListener(this);
@@ -69,7 +59,6 @@ public class Qrcode extends AppCompatActivity implements View.OnClickListener {
 
             @Override
             public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
             }
 
             @Override
@@ -87,24 +76,24 @@ public class Qrcode extends AppCompatActivity implements View.OnClickListener {
             @Override
             public void receiveDetections(Detector.Detections<Barcode> detections) {
                 SparseArray<Barcode> qrCodes = detections.getDetectedItems();
-                if(qrCodes.size() != 0)
-                {
+                if(qrCodes.size() != 0) {
                     textView.post(new Runnable() {
                         @Override
                         public void run() {
 
                             if (stop == true) {
-
-                                if(qrCodes.valueAt(0).displayValue.length() != 15){
+                                textView.setText(qrCodes.valueAt(0).displayValue);
+                                String result = textView.getText().toString();
+                                if(result.length() != 15){
                                     Toast.makeText(getApplicationContext(),"Qrcode ของคุณไม่ใช่ของลอตเอตรี่", Toast.LENGTH_LONG).show();
                                     Intent resultIntent = new Intent();
                                     setResult(RESULT_CANCELED,resultIntent);
                                     finish();
+                                    return;
                                 }
-                                textView.setText(qrCodes.valueAt(0).displayValue);
-                                String result = textView.getText().toString();
                                 String lot_year = result.substring(0,2);
-
+                                String lot_time = "";
+                                lot_time += result.substring(3,5);
                                 if(result.charAt(2) != '-'){
                                     Toast.makeText(getApplicationContext(),"2", Toast.LENGTH_LONG).show();
                                     return;
@@ -117,15 +106,19 @@ public class Qrcode extends AppCompatActivity implements View.OnClickListener {
                                     Toast.makeText(getApplicationContext(),"8", Toast.LENGTH_LONG).show();
                                     return;
                                 }
-                                int[] Checktextqr = {0,1,2,3,4,6,7,9,10,11,12,13,14};
+                                int[] Checktextqr = {0,1,3,4,6,7,9,10,11,12,13,14};
                                 for(int i =0; i < Checktextqr.length;i++){
                                     if (result.charAt(Checktextqr[i]) < '0' ||result.charAt(Checktextqr[i]) >'9'){
-                                        Toast.makeText(getApplicationContext(),"Qrcode ของคุณไม่ใช่ลอตเตอรี่", Toast.LENGTH_LONG).show();
+                                        Toast.makeText(getApplicationContext(),Checktextqr[i], Toast.LENGTH_LONG).show();
                                         return;
                                     }
                                 }
-                                if (lot_year.equals("62") == false && lot_year.equals("61") == false){
+                                if (!lot_year.equals("62") && !lot_year.equals("61")){
                                     Toast.makeText(getApplicationContext(),"ขออภัยไม่สามรถเก็บ วันที่ และ ข้อมูล ดังกล่าวได้", Toast.LENGTH_LONG).show();
+                                    return;
+                                }
+                                if (Integer.parseInt(lot_time) > 48 ||Integer.parseInt(lot_time) == 0){
+                                    Toast.makeText(getApplicationContext(),"Qrcode ของคุณไม่ใช่ลอตเตอรี่+", Toast.LENGTH_LONG).show();
                                     return;
                                 }
                                 String text = textView.getText().toString().substring(9);
@@ -160,10 +153,6 @@ public class Qrcode extends AppCompatActivity implements View.OnClickListener {
                 }
             }
         });
-    }
-
-    public void takeQrCode() {
-        startActivity(new Intent(this, Qrcode.class));
     }
 
     @Override
@@ -209,20 +198,14 @@ public class Qrcode extends AppCompatActivity implements View.OnClickListener {
                 AlertDialog.Builder altdial = new AlertDialog.Builder(Qrcode.this);
                 altdial.setMessage(text+"\nใช่เลขที่คุณต้องการหรือไม่?");
                 altdial.setCancelable(false);
-                altdial.setPositiveButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        return;
-                    }
+                altdial.setPositiveButton("No", (dialog, which) -> {
+                    return;
                 });
-                altdial.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Intent resultIntent = new Intent();
-                        resultIntent.putExtra("result",textView.getText());
-                        setResult(RESULT_OK,resultIntent);
-                        finish();
-                    }
+                altdial.setNegativeButton("Yes", (dialog, which) -> {
+                    Intent resultIntent = new Intent();
+                    resultIntent.putExtra("result",textView.getText());
+                    setResult(RESULT_OK,resultIntent);
+                    finish();
                 });
                 AlertDialog alert = altdial.create();
                 alert.setTitle("Record");
